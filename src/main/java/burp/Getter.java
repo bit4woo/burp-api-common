01043fe41119a3300958e371f37eae72d6a4742f
bit4woo <bit4woo@163.com>
2019-08-09 09:43:47 +0800

    Update Getter.java

--- src/main/java/burp/Getter.java
+++ src/main/java/burp/Getter.java
@@ -3,7 +3,6 @@ package burp;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map.Entry;
